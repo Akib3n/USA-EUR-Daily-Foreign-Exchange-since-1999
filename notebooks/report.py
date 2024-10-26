de22d08658007444e71e10308225d998ad87d54b
Akiben <stevenlouiedejesus@gmail.com>
2024-10-26 20:43:24 +0800

    chore: report.py documentation

--- notebooks/report.py
+++ notebooks/report.py
@@ -1,17 +1,18 @@
 """ DATASET URL: https://datahub.io/core/exchange-rates#daily """
 
 """ 
-**I. Import Libraries **
+**I. Import Libraries**
 """
 import os
 import pandas as pd
 import numpy as np
 
-""" 
+"""
 **II. Exploratory Data Analysis**
 """
 # Get Dataset
 Dataset_url = 'https://raw.githubusercontent.com/Akib3n/USA-EUR-Daily-Foreign-Exchange-since-1999/main/daily.csv'
+
 
 # Load the dataset
 Dataset = pd.read_csv(Dataset_url)
@@ -19,6 +20,9 @@ Dataset = pd.read_csv(Dataset_url)
 # Convert 'Exchange rate' column to numeric
 Dataset['Exchange rate'] = pd.to_numeric(Dataset['Exchange rate'], errors='coerce')
 
+"""
+**III. Data Cleaning and Preprocessing
+"""
 # Remove Rows with empty exchange rate
 Dataset = Dataset.dropna(subset=['Exchange rate'])
 
