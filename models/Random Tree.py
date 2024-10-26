b102517d7581cb2b301efea501bc01660457e6b3
Akiben <stevenlouiedejesus@gmail.com>
2024-10-26 10:54:24 +0800

    feat: preprocessed dataset

---
+++ models/Random Tree.py
@@ -0,0 +1,10 @@
+# Import Libraries
+import os
+import pandas as pd
+import numpy as np
+
+# Import preprocessed dataset
+preprocessed = 'datasets/preprocessed.csv'
+
+# Load the dataset
+fexchange = pd.read_csv(preprocessed)
